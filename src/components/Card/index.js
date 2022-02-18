b863651d9d40093ac1888732af4135f0ab4a4798
Guilherme Couto <gcoutos95@gmail.com>
2022-02-18 16:07:13 -0300

    feat: Adding the final solution

---
+++ src/components/Card/index.js
@@ -0,0 +1,48 @@
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import qrcode from "../../assets/images/qr-code.png";
+
+import { motion } from "framer-motion";
+
+const MotionCard = motion(Box);
+
+export const Card = () => {
+  return (
+    <MotionCard
+      animate={{
+        y: [-5, 0],
+        opacity: [0, 1],
+      }}
+      transition={{ duration: 1 }}
+      padding="20px"
+      backgroundColor="white"
+      boxShadow="0 0 10px rgba(0, 0, 0, 0.2)"
+      borderRadius="10px"
+    >
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        w="220px"
+        mb="15px"
+        borderRadius="15px"
+        backgroundColor="blue.300"
+      >
+        <Image src={qrcode} borderRadius="10px" alt="qrcode" />
+      </Flex>
+
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        w="220px"
+        textAlign="center"
+      >
+        <Text mb="10px" fontWeight="semibold">
+          Improve your front-end skills by building projects
+        </Text>
+        <Text fontSize="xs" fontWeight="medium" color="gray.500">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </Text>
+      </Flex>
+    </MotionCard>
+  );
+};
---
+++ src/components/Creator/index.js
@@ -0,0 +1,47 @@
+import { Flex, Link, Text } from "@chakra-ui/react";
+
+import { motion } from "framer-motion";
+
+const FlexMotion = motion(Flex);
+
+export const Creator = () => {
+  return (
+    <FlexMotion
+      animate={{
+        opacity: [0, 1],
+      }}
+      transition={{ duration: 1 }}
+      position="absolute"
+      bottom="0"
+      padding="10px"
+      alignItems="center"
+      fontSize="sm"
+    >
+      <Text fontSize="xs">
+        Made with 🖤 by{" "}
+        <Link
+          href="https://github.com/GuiCoutoSt"
+          target="_blank"
+          _hover={{
+            textDecoration: "none",
+          }}
+        >
+          <Text
+            as="span"
+            padding="2px"
+            fontWeight="medium"
+            _hover={{
+              borderRadius: "3px",
+              textDecoration: "none",
+              backgroundColor: "black",
+              color: "white",
+              transition: "0.3s",
+            }}
+          >
+            GuiCoutoSt
+          </Text>
+        </Link>{" "}
+      </Text>
+    </FlexMotion>
+  );
+};
